1670a87747c0d8a07936723c4c78cd61b852aa49
jamesbyrne113 <jamesbyrne113@gmail.com>
2020-06-23 14:24:40 +0100

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by James Byrne on 23/06/2020.
+//  Copyright © 2020 jamesbyrne. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
